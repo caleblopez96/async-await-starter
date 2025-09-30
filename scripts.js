ac5fb7da5c617fd6aa782ac99e6a19c99529630f
caleblopez96 <caleblopez96@gmail.com>
2025-09-30 15:53:03 -0700

    completed assignment

--- scripts.js
+++ scripts.js
@@ -1,7 +1,6 @@
 // helper function to semantically display the returned data
-function displayUsers(output, id, data){
+function displayUsers(output, id, data) {
     // determine which container on the page we'll be dealing with/the call type
-    
 
     // to track the currently displayed page
     let currentPage = 1;
@@ -22,17 +21,23 @@ function displayUsers(output, id, data){
     let page4 = [];
 
     // split the returned data into four arrays, each will represent a "page" of users with three on each page
-    // TO DO: divide the data returned into four arrays
-
+    for (let i = 0; i < 12; i++) {
+        if (i < 3) {
+            page1.push(data.results[i]);
+        } else if (i < 6) {
+            page2.push(data.results[i]);
+        } else if (i < 9) {
+            page3.push(data.results[i]);
+        } else {
+            page4.push(data.results[i]);
+        }
+    }
     // displays each user in a section, with semantic markup for their name, email address, and image
-    function displayPage(currentPage){
-        // TO DO - complete the code to display the JSON data to the page
-
+    function displayPage(currentPage) {
         // iterate through the data for the page's array and display it on the page
-        for(let user of currentPage){
-        
+        for (let user of currentPage) {
             // create a container to store each user's content as we build our output
-            let userSection = document.createElement("section");            
+            let userSection = document.createElement("section");
 
             // add a class to that section/each section containing a user
             userSection.classList.add("user");
@@ -41,31 +46,30 @@ function displayUsers(output, id, data){
             let profilePhoto = document.createElement("img");
 
             // set the image attributes so that it will display the correct image
-            profilePhoto.src = `${"TO DO"}`;
-            profilePhoto.alt = `${"TO DO"} ${"TO DO"}`;
-            
+            profilePhoto.src = `${user.picture.large}`;
+            profilePhoto.alt = `${user.name.first} ${user.name.last}`;
+
             // add the image to the current user section
             userSection.appendChild(profilePhoto);
 
             // create the heading to store the user name in each section
             let userName = document.createElement("h3");
-            
+
             // add the name to the element
-            userName.textContent = `${"TO DO"} ${"TO DO"}`;
-            
+            userName.textContent = `${user.name.first} ${user.name.last}`;
+
             // add the name to the section after the image
             userSection.appendChild(userName);
 
             // create the email address link for the user in each section
             let emailAddress = document.createElement("a");
-            
+
             // set the link text and attributes
-            emailAddress.href = `mailto:${"TO DO"}`;
-            emailAddress.textContent = `${"TO DO"}`;
-            
+            emailAddress.href = `mailto:${user.email}`;
+            emailAddress.textContent = `${user.email}`;
+
             // add the email address to the section
             userSection.appendChild(emailAddress);
-            
 
             // add the completed user section to the page before the pagination controls
             userContainer.appendChild(userSection);
@@ -79,32 +83,30 @@ function displayUsers(output, id, data){
     let buttons = document.querySelectorAll("#" + id + " .btn");
 
     // function to adjust the current-page class based on parameters passed in (current page)
-    function updateBtnStylesAndAria(newPage){
+    function updateBtnStylesAndAria(newPage) {
         // update the previous and next buttons, depending on which page we're moving to
-        if(newPage === 1){
+        if (newPage === 1) {
             // update the aria-label on the previous link
             buttons[0].ariaLabel = "Go to the Previous page, page 4";
 
             // update the aria-label on the next link
             buttons[5].ariaLabel = "Go to the Next page, page 2";
-
-        }else if(newPage === 4){
+        } else if (newPage === 4) {
             // update the aria-label on the previous link
             buttons[0].ariaLabel = "Go to the Previous page, page 3";
 
             // update the aria-label on the next link
             buttons[5].ariaLabel = "Go to the Next page, page 1";
-        }else{
+        } else {
             // update the aria-label on the previous link
             buttons[0].ariaLabel = "Go to the Previous page, page " + (newPage - 1);
 
             // update the aria-label on the next link
             buttons[5].ariaLabel = "Go to the Next page, page " + (newPage + 1);
-
         }
 
         // for all of the numbered buttons, remove the current page class
-        for(let i = 1; i < 5; i++){
+        for (let i = 1; i < 5; i++) {
             buttons[i].classList.remove("current-page");
             buttons[i].ariaCurrent = "false";
             buttons[i].ariaLabel = "Go to page " + i;
@@ -122,27 +124,27 @@ function displayUsers(output, id, data){
     updateBtnStylesAndAria(1);
 
     // handle the user clicking to visit the previous page
-    buttons[0].addEventListener("click", function(e){
+    buttons[0].addEventListener("click", function (e) {
         // prevent default link behavior
         e.preventDefault();
 
         // determine the current page, then call to display the previous one
-        if(currentPage === 1){
+        if (currentPage === 1) {
             userContainer.innerHTML = "";
             displayPage(page4);
             currentPage = 4;
             updateBtnStylesAndAria(4);
-        }else if(currentPage === 2){
+        } else if (currentPage === 2) {
             userContainer.innerHTML = "";
             displayPage(page1);
             currentPage = 1;
             updateBtnStylesAndAria(1);
-        }else if(currentPage === 3){
+        } else if (currentPage === 3) {
             userContainer.innerHTML = "";
             displayPage(page2);
             currentPage = 2;
             updateBtnStylesAndAria(2);
-        }else{
+        } else {
             userContainer.innerHTML = "";
             displayPage(page3);
             currentPage = 3;
@@ -151,10 +153,10 @@ function displayUsers(output, id, data){
     });
 
     // handle when the user clicks to view page 1
-    buttons[1].addEventListener("click", function(e){
+    buttons[1].addEventListener("click", function (e) {
         // prevent default link behavior
         e.preventDefault();
-        
+
         userContainer.innerHTML = "";
         displayPage(page1);
         currentPage = 1;
@@ -162,10 +164,10 @@ function displayUsers(output, id, data){
     });
 
     // handle when the user clicks to view page 2
-    buttons[2].addEventListener("click", function(e){
+    buttons[2].addEventListener("click", function (e) {
         // prevent default link behavior
         e.preventDefault();
-        
+
         userContainer.innerHTML = "";
         displayPage(page2);
         currentPage = 2;
@@ -173,10 +175,10 @@ function displayUsers(output, id, data){
     });
 
     // handle when the user clicks to view page 3
-    buttons[3].addEventListener("click", function(e){
+    buttons[3].addEventListener("click", function (e) {
         // prevent default link behavior
         e.preventDefault();
-        
+
         userContainer.innerHTML = "";
         displayPage(page3);
         currentPage = 3;
@@ -184,10 +186,10 @@ function displayUsers(output, id, data){
     });
 
     // handle when the user clicks to view page 4
-    buttons[4].addEventListener("click", function(e){
+    buttons[4].addEventListener("click", function (e) {
         // prevent default link behavior
         e.preventDefault();
-        
+
         userContainer.innerHTML = "";
         displayPage(page4);
         currentPage = 4;
@@ -195,27 +197,27 @@ function displayUsers(output, id, data){
     });
 
     // handle the user clicking to visit the next page
-    buttons[5].addEventListener("click", function(e){
+    buttons[5].addEventListener("click", function (e) {
         // prevent default link behavior
         e.preventDefault();
-        
+
         // determine the current page, then call to display the previous one
-        if(currentPage === 1){
+        if (currentPage === 1) {
             userContainer.innerHTML = "";
             displayPage(page2);
             currentPage = 2;
             updateBtnStylesAndAria(2);
-        }else if(currentPage === 2){
+        } else if (currentPage === 2) {
             userContainer.innerHTML = "";
             displayPage(page3);
             currentPage = 3;
             updateBtnStylesAndAria(3);
-        }else if(currentPage === 3){
+        } else if (currentPage === 3) {
             userContainer.innerHTML = "";
             displayPage(page4);
             currentPage = 4;
             updateBtnStylesAndAria(4);
-        }else{
+        } else {
             userContainer.innerHTML = "";
             displayPage(page1);
             currentPage = 1;
@@ -227,11 +229,27 @@ function displayUsers(output, id, data){
 // https://randomuser.me/api/?results=12&nat=us,gb
 
 // fetch call to the API
-// TO DO - Complete the code to call the API and display the returned data on the page in the correct place
-
+fetch("https://randomuser.me/api/?results=12&nat=us,gb")
+    .then((res) => res.json())
+    .then((data) => {
+        console.log(data);
+        displayUsers("userPage1", "pagination1", data);
+    })
+    .catch((err) => console.error(err));
 
 // async/await call to the same API
-// TO DO - Complete the code to call the API using an async function
+async function getUsers() {
+    let res = await fetch("https://randomuser.me/api/?results=12&nat=us,gb");
+    if (res.error) {
+        throw new Error(res.error);
+    }
+    return await res.json();
+}
 
-// call our async function and handle the returned promise 
-// TO DO - Complete the code to handle the data returned from the API and display the returned data on the page in the correct place
+// call our async function and handle the returned promise
+getUsers()
+    .then((json) => {
+        console.log(json);
+        displayUsers("userPage2", "pagination2", json);
+    })
+    .catch((err) => console.error(err.message));
